6b12b366a4493fdcc8c9fc8c5527889f2edbda73
Mayank Jain <immanku7@gmail.com>
2025-11-16 11:52:04 +0530

    updated for init

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -5,7 +5,7 @@ PDF loading, metadata extraction, and vector database operations.
 """
 
 from .pdf_loader import PDFLoader
-#from .metadata_extractor import MetadataExtractor
+from .metadata_extractor import MetadataExtractor
 from .vector_db import VectorDatabase
 
-__all__ = ['PDFLoader', 'VectorDatabase']
+__all__ = ['PDFLoader', 'MetadataExtractor', 'VectorDatabase']
